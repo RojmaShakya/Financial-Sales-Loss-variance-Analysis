e50a103121dfc4e69ed7aee090d80d964c18ba89
RojmaShakya <shakyarojma91@gmail.com>
2026-09-14 19:41:56 +0545

    Update R file

--- Financial.R
+++ Financial.R
@@ -1,17 +1,17 @@
-#Which segments are making losses, and how much?
+
 # Load the cleaned financial data
 df <- read.csv("Financials_cleaned.csv")
 
-#Which transactions lost money?
+
 # Keep only transactions where the company made a loss (Profit is negative)
 losses <- subset(df, Profit < 0)
 
-#What is the company's total loss?
-#Give me the Profit column from the losses table.
+
+
 total_loss <- sum(losses$Profit) 
 
 #Which segment lost the most money?
-#from sql query
+
 loss_by_segment <- aggregate(
   Profit ~ Segment,
   data = losses,
@@ -30,5 +30,5 @@ loss_summary <- merge(loss_count, loss_by_segment, by = "Segment")
 names(loss_summary) <- c("Segment", "Loss_Transactions", "Total_Loss")
 print(loss_summary)
 
-# Ctrl+Shift+P
+
 # source("financial.R")   
